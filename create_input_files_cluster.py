c1087fbafba3854c773382cf29109e7a9bd4061a
apoorva gokhale <agokhale@dsmlp-login.ucsd.edu>
2019-12-01 16:28:34 -0800

    cluster running specs added

---
+++ create_input_files_cluster.py
@@ -0,0 +1,11 @@
+from utils import create_input_files
+
+if __name__ == '__main__':
+    # Create input files (along with word map)
+    create_input_files(dataset='coco',
+                       karpathy_json_path='../andrejsplits/dataset_coco.json',
+                       image_folder='/apoorva-volume/images.cocodataset.org/zips',
+                       captions_per_image=5,
+                       min_word_freq=5,
+                       output_folder='/apoorva-volume/captioning/data_generated',
+                       max_len=50)

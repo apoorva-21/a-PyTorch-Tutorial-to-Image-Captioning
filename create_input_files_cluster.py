3dab7d45ede1ec7929f50d2328ff6b49ece07e32
Apoorva Gokhale <apoorvagokhale89@gmail.com>
2019-12-01 17:38:47 -0800

    Update create_input_files_cluster.py

--- create_input_files_cluster.py
+++ create_input_files_cluster.py
@@ -3,7 +3,7 @@ from utils import create_input_files
 if __name__ == '__main__':
     # Create input files (along with word map)
     create_input_files(dataset='coco',
-                       karpathy_json_path='../andrejsplits/dataset_coco.json',
+                       karpathy_json_path='/apoorva-volume/captioning/andrejsplits/dataset_coco.json',
                        image_folder='/apoorva-volume/images.cocodataset.org/zips',
                        captions_per_image=5,
                        min_word_freq=5,
